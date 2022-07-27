0d9350f4c2bb7edcf444401445fc7c223b4eec71
Arie Brainware <publicu001@protonmail.com>
2022-07-27 15:05:21 +0700

    Test adjust post data form

--- wifi-scanner-last-version/wifi-scanner-last-version.ino
+++ wifi-scanner-last-version/wifi-scanner-last-version.ino
@@ -1,86 +1,141 @@
+
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 
-const char* ssid = ""; //Nama SSID AP/Hotspot
-const char* password = ""; //Password Wifi
+// konfigurasi jaringan wifi
+const char* ssid = "PALA-Guest";
+const char* password = "wifitamu";
 
-ESP8266WebServer server(80); //Menyatakan Webserver pada port 80
+#define PIN_LED 2             // inisialisasi LED di pin 2
+
+const int RSSI_MAX =-50;      // menentukan kekuatan sinyal maksimum dalam dBm
+const int RSSI_MIN =-100;     // menentukan kekuatan sinyal minimum dalam dBm
+
+String r = "Mencari Sinyal WiFi...";
+String s = "Jaringan tidak ditemukan.";
+String t = "  jaringan ditemukan.";
+String u = "";
+String za = "\n";
 
 void setup() {
-  Serial.begin(9600);
-  delay(10);
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        
+  delay(1000);
+  WiFi.mode(WIFI_STA);        // Set WiFi ke mode stasiun
+  //WiFi.disconnect();
+  delay(2000);
+  pinMode(PIN_LED, OUTPUT);
 
-// Connect ke WiFi
+  // setting koneksi
   Serial.println();
   Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print("Koneksi ke ");
   Serial.println(ssid);
 
-// Mengatur WiFi 
-  WiFi.mode(WIFI_STA); // Mode Station
-  WiFi.begin(ssid, password); // Mencocokan SSID dan Password
-    
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  WiFi.begin(ssid, password);
+
+  // cek koneksi
+  while(WiFi.status() != WL_CONNECTED){
+      
+      digitalWrite(PIN_LED, HIGH);     // LED mati
+
+      // NodeMCU akan terus mencoba koneksi
+      Serial.print(".");
+      delay(500);
   }
 
-// Tampilkan status Connected
+  // bila terkoneksi
+  digitalWrite(PIN_LED, LOW);          // LED menyala
+  // tampilkan di serial monitor
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi Terkoneksi");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-// Membuat tampilan Web
-  server.on("/", []() {
-    server.send(200, "text/plain", "Hello Para Warrior IoT");
-  });
-
-// Bagian ini untuk membuat halaman web lain
-  server.on("/status", []() {
-    int n = WiFi.scanNetworks();
-    
-    Serial.println("Scan done");
-
-  String response = "";
-  if (n == 0)
-  {
-    Serial.println("No Networks Found");
-    response = "[]";
-  }
-  else
-  {
-    Serial.print(n);
-    Serial.println(" Networks found");
-
-//    response = "{";
-    response+="[";
-    for (int i = 0; i < n; ++i)
-    {   
-      response+="{'SSID':'";
-      response+=WiFi.SSID(i);
-      response+="',";
-      response+="'RSSI':'";
-      response+=WiFi.RSSI(i);
-      response+="',";
-      response+="'MAC':'";
-      response+=WiFi.BSSIDstr(i);
-      response+="',";
-      response+="'isSecured':";
-      response+=(WiFi.encryptionType(i) == ENC_TYPE_NONE)?"'Unsecured'":"'Secured'";
-      response+="}";
-      response+=(n-i != 1 ? ",":"");
-    }
-    response+="]";
-//    response+="}";
-  }
-    server.send(200, "application/json", response);
-  });
-
-  server.begin();
-  Serial.println("Webserver dijalankan");
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
 }
 
 void loop() {
-   server.handleClient();
+  Serial.println(r);
+  Serial.println("");
+  //kirimData(r);
+
+  int n = WiFi.scanNetworks();
+  String sn = String(n);
+  
+  if (n == 0) {
+    Serial.println("Jaringan tidak ditemukan.");
+    //kirimData(s);
+  } else {
+    String a = sn + t + za + za;
+    Serial.print(n);
+    Serial.println(t);
+    
+    u = "";
+    for (int i = 0; i < n; ++i) {
+      int v = i + 1;
+      String nv = String(v) + ") ";
+      String w = String(WiFi.SSID(i)) + "  ";
+      String x = String(WiFi.RSSI(i)) + "dBm (";
+      String xv = String(dBmtoPercentage(WiFi.RSSI(i)));
+      String xw = String("%)");
+      String y = nv + w + x + xv + xw + za;
+      u = u + y;
+
+      Serial.print("  ");
+      Serial.print(nv);
+      Serial.print(w);
+      Serial.print(x);
+      Serial.print(dBmtoPercentage(WiFi.RSSI(i)));
+      Serial.println("%)");
+
+      delay(10);
+    }
+    String zu = a + u;
+    kirimData(zu);
+    u = "";
+    zu = "";
+  }
+  Serial.println("");
+
+  delay(2000);
+  WiFi.scanDelete();  
+}
+
+void kirimData(String isi) {
+    
+    String postData = (String)"data=" + isi;
+    
+    // cek koneksi ke server
+    HTTPClient http;
+    WiFiClient wc;
+    http.begin(wc, "http://192.168.1.6:3110/result");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    Serial.println("----- ISI DATA -----");
+    Serial.println("data"+isi);
+    //String data = "value1=24.25&value2=49.54&value3=1005.14";
+    String data = "data"+String(isi);
+    auto httpCode = http.POST(data);
+    String isiData = http.getString();     // Mengambil data post yang dikirim
+    
+    http.end();
+    delay(700);
+}
+
+// Kekuatan sinyal dalam %
+int dBmtoPercentage(int dBm) {
+  int quality;
+    if(dBm <= RSSI_MIN) {
+        quality = 0;
+    } else if(dBm >= RSSI_MAX) {  
+        quality = 100;
+    } else {
+        quality = 2 * (dBm + 100);
+    }
+    return quality;
 }
